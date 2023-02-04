c0d1d62b5d297bcdc18a98a7a538f68eb04b1919
direktor2007 <>
2023-02-04 14:53:58 +0700

    test BBB

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите число:"); // Вывод строки в терминал
+int number = Convert.ToInt32(Console.ReadLine() );
+int result = number * number; // Вычисление квадрата
+System.Console.WriteLine(result); 
